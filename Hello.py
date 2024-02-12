aef400d89b01261fb5be78faa37fb39e9552fe49
Manoj Kumar <90456319+Manojkumar1709@users.noreply.github.com>
2024-02-12 11:19:24 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,5 @@
 import datetime
 
 current_time = datetime.datetime.now()
-print("Hello, World")
+print("Hello, Welcome to Python Basic Hello World Application")
 print("Current Time is:", current_time)
